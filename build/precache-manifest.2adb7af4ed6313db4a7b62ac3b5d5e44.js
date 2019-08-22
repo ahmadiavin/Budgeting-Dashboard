181a4aeead5b0c7bb96679b539ced577ff51e71c
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-08-22 14:35:05 -0500

    build

---
+++ build/precache-manifest.2adb7af4ed6313db4a7b62ac3b5d5e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0747c2f110a3eab85451c9326e8a2558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c18d41974186067f1b",
+    "url": "/static/css/main.788d3c20.chunk.css"
+  },
+  {
+    "revision": "23923a07e538e92b2b6f",
+    "url": "/static/js/2.7b9df99f.chunk.js"
+  },
+  {
+    "revision": "30c18d41974186067f1b",
+    "url": "/static/js/main.1ae7f28e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c12bfecc22ca80e4eaf7e5fd7ccf2ac8",
+    "url": "/static/media/leaf.c12bfecc.svg"
+  },
+  {
+    "revision": "6cbe7ef445c3a60bfcd12fce0252d339",
+    "url": "/static/media/overlay.6cbe7ef4.png"
+  }
+]);
